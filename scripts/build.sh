7ac64c2816432739787d1f026ae13bf933266e2b
Nick Pierson <me@nick.exposed>
2020-08-20 02:27:20 +0000

    add osrs,rs3 Dockerfiles

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,14 @@ set -ex
 USERNAME=ncpierson
 IMAGE=fashionscape-web
 
-docker build                 \
-  --no-cache                 \
-  -t $USERNAME/$IMAGE:latest \
+docker build                     \
+  --no-cache                     \
+  -f builds/oldschool/Dockerfile \
+  -t $USERNAME/$IMAGE:latest     \
+  .
+
+docker build                           \
+  --no-cache                           \
+  -f builds/runescape/Dockerfile       \
+  -t $USERNAME/$IMAGE:latest-runescape \
   .
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,5 +9,11 @@ VERSION=`cat VERSION`
 ./scripts/build.sh
 
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION-oldschool
 docker push $USERNAME/$IMAGE:latest
 docker push $USERNAME/$IMAGE:$VERSION
+docker push $USERNAME/$IMAGE:$VERSION-oldschool
+
+docker tag $USERNAME/$IMAGE:latest-runescape $USERNAME/$IMAGE:$VERSION-runescape
+docker push $USERNAME/$IMAGE:latest-runescape
+docker push $USERNAME/$IMAGE:$VERSION-runescape
