3eec38bab81d9528cdb6f46ff27b3a48bf3e5150
Nick Pierson <me@nick.exposed>
2020-08-29 19:38:31 +0000

    update header based on domain

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import config from '../config';
+
 import './Header.css';
 
 const Header = () => {
+  return config.release === 'runescape' ? (
+    <RunescapeHeader />
+  ) : (
+    <OldSchoolHeader />
+  );
+};
+
+const OldSchoolHeader = () => {
   return (
     <div className="header">
       <h1 className="title">
@@ -13,6 +23,23 @@ const Header = () => {
         <img
           alt="gnome scarf"
           src="https://oldschool.runescape.wiki/images/3/3a/Gnome_scarf.png?2d77d"
+        />
+      </p>
+    </div>
+  );
+};
+
+const RunescapeHeader = () => {
+  return (
+    <div className="header">
+      <h1 className="title">
+        <a href="https://rune.scape.fashion">rune.scape.fashion</a>
+      </h1>
+      <p className="subtitle">
+        A fashionscape tool for Runescape{' '}
+        <img
+          alt="gnome scarf"
+          src="https://runescape.wiki/images/3/3a/Gnome_scarf.png?5a375"
         />
       </p>
     </div>
